acd97e17704717a6895a7ab7db80f12902544e9d
kasiviswanathanR <kasiviswanathanramesh@gmail.com>
2024-07-09 18:57:11 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    base: '/portfolio/', 
     outDir: 'build', // Change this to your desired output directory
   },
   plugins: [react()],
