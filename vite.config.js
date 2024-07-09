0d3f63d110880b62887b9a93ddcc3e54677941d4
kasiviswanathanR <kasiviswanathanramesh@gmail.com>
2024-07-09 18:51:03 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build', // Change this to your desired output directory
+  },
   plugins: [react()],
 })
